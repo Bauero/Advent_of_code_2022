8fcdc3a9b50f7d17da41c5d48d895dc5ae70f58d
Piotr Bauer <piotr.bauer.konto@gmail.com>
2022-12-09 09:44:30 +0100

    Poprawka do maina - zapisanie danych na wstępnie od razu do tablicy

--- dzien8.py
+++ dzien8.py
@@ -2,10 +2,23 @@
 
 #poznanie szerokości i wysokości naszego lasu
 f = open("input8.txt","r")
-szerokosc = len(f.readline())
+linijka = f.readline()
+szerokosc = len(linijka)
+lista_danych = [list(linijka.rstrip().lstrip())]
 wysokosc = 1
 for i in f:
     wysokosc+=1
+    lista_danych.append(list(i.rstrip().lstrip()))
 f.close()
 
-#rozwiązanie 1 - brute force
+ilosc_dobrych_drzew = 0
+
+for i in range(len(wysokosc)):
+    for j in range(len(szerokosc)):
+
+        #element znajduje się na krawędzi
+        if i == 0 or i == len(lista_danych) or j == 0 or j == len(lista_danych[i]):
+            continue
+        else:
+            #przetestowanie widoczności we wszystkich kierunkach
+
