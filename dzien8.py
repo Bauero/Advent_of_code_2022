21db5434fffb4678a41da8a79e7137468466a42d
Piotr Bauer <piotr.bauer.konto@gmail.com>
2022-12-09 20:33:21 +0100

    naprawiam

--- dzien8.py
+++ dzien8.py
@@ -17,3 +17,34 @@ kopia_lista_danych = copy.deepcopy(lista_danych)
 
 ilosc_dobrych_drzew = 0
 
+for r in range(1,wysokosc-1):
+    for k in range(1,szerokosc-2):
+
+        obecny_element = lista_danych[r][k]
+
+        lewo = lista_danych[r][:k]
+        prawo = lista_danych[r][k+1:]
+        kolumna = [lista_danych[x][k] for x in range(wysokosc)]
+        gora = kolumna[:r]
+        dol = kolumna[r+1:]
+        
+        #print("Lewo ",lewo)
+        #print("Prawo ",prawo)
+        #print("Góra ",gora)
+        #print("Dół ",dol)
+
+
+        if max(lewo) < obecny_element or max(prawo) < obecny_element \
+            or max(gora) < obecny_element or max(dol) < obecny_element:
+            continue
+
+        ilosc_dobrych_drzew +=1
+
+        kopia_lista_danych[r][k] = "*"
+
+f = open("wynik8.txt","w")
+
+for i in kopia_lista_danych:
+    napis = ""
+    for j in range(len(i)):
+        napis += str(j)
